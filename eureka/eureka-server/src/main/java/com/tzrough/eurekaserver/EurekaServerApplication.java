0ebd7700b8c9603dde522936706bfc0cc3d64c83
blackz <zhanghongfei@nequal.com>
2019-08-02 17:59:43 +0800

    add eureka client

--- eureka/eureka-server/src/main/java/com/tzrough/eurekaserver/EurekaServerApplication.java
+++ eureka/eureka-server/src/main/java/com/tzrough/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.focus.tzrough.eurekaserver;
+package com.tzrough.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka/eureka-server/src/test/java/com/tzrough/eurekaserver/EurekaServerApplicationTests.java
+++ eureka/eureka-server/src/test/java/com/tzrough/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.focus.tzrough.eurekaserver;
+package com.tzrough.eurekaserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
